d6a1b531ccfd1c41dcf2cfbd42c69d9b23e3bcc6
rafquint <rafquint@gmail.com>
2022-05-22 11:45:30 -0300

    Setting UniqueConstraint to StudentCourse with "student_id" and "course_id"

--- src/main/java/br/com/rqgr/srs/model/StudentCourse.java
+++ src/main/java/br/com/rqgr/srs/model/StudentCourse.java
@@ -6,6 +6,7 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,7 +22,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "student_course")
+@Table(name = "student_course",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"student_id", "course_id"})
+        }
+)
 @XmlRootElement
 public class StudentCourse extends AbstractEntity {
 
